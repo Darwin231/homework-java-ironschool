1929fdf8a1e27ab2049e5ab666d48d3cc95d3e08
jtiradohernandez <javiertirhe@gmail.com>
2024-02-29 10:59:58 +0100

    Solución para el selector de comandos

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -1,5 +1,7 @@
+import javax.sound.midi.SysexMessage;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Commands {
     private Map<String, Course> courseList = new HashMap<>();
@@ -12,6 +14,7 @@ public class Commands {
         setStudentList(studentList);
         setTeacherList(teacherList);
     }
+
 
     public Map<String, Course> getCourseList() {
         return courseList;
@@ -37,34 +40,87 @@ public class Commands {
         this.teacherList = teacherList;
     }
 
-    public void commandSelector(CommandEnum commandAction){
+
+    // TODO mover el metodo a otra clase
+    public void selector(){
+        System.out.println("Choose a command:\n");
+        System.out.println("1. Enroll:\n");
+        System.out.println("2. Assign:\n");
+        System.out.println("3. Show courses:\n");
+        System.out.println("4. Lookup course:\n");
+        System.out.println("5. Show students:\n");
+        System.out.println("6. Lookup student\n");
+        System.out.println("7. Show teachers:\n");
+        System.out.println("8. Lookup teacher:\n");
+        System.out.println("9. Show profit:\n");
+        Scanner scanner = new Scanner(System.in);
+        int commandoption = scanner.nextInt();
+        commandSelector(CommandEnum.ENROLL, scanner);
+    }
+    public void commandSelector(CommandEnum commandAction, Scanner scanner){
         String studentID = "asdfasdf";
         String courseID = "asdfasdf";
         String teacherID = "asdfasdf";
         switch (commandAction){
         case ENROLL:
+            System.out.println("Which course do you want to enroll? (write the id):\n");
+            courseList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName() + " ");
+            });
+            courseID = scanner.next();
+            System.out.println("Which student do you want to enroll in this course? (write the id):\n");
+            studentList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName()+ " ");
 
+            });
+            studentID = scanner.next();
             enroll(studentID,courseID);
             break;
         case ASSIGN:
+            System.out.println("In which course do you want to assign a teacher? (write the id):\n");
+            courseList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName() + " ");
+            });
+            courseID = scanner.next();
+            System.out.println("Which teacher do you want to assign to this course? (write the id):\n");
+            teacherList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName()+ " ");
+
+            });
+            studentID = scanner.next();
             assign(teacherID,courseID);
             break;
         case SHOW_COURSES:
             ShowCourses();
             break;
         case LOOKUP_COURSE:
+            System.out.println("Which course do you want to look up? (write the id):\n");
+            courseList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName() + " ");
+            });
+            courseID = scanner.next();
             LookupCourse(courseID);
             break;
         case SHOW_STUDENTS:
             ShowStudents();
             break;
         case LOOKUP_STUDENT:
+            System.out.println("Which student do you want to look up? (write the id):\n");
+            studentList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName() + " ");
+            });
+            studentID = scanner.next();
             LookupStudent(studentID);
             break;
         case SHOW_TEACHERS:
             ShowTeachers();
             break;
         case LOOKUP_TEACHER:
+            System.out.println("Which teacher do you want to look up? (write the id):\n");
+            teacherList.forEach((key,value) -> {
+                System.out.println(key + " = " + value.getName() + " ");
+            });
+            teacherID = scanner.next();
             LookupTeacher(teacherID);
             break;
         case SHOW_PROFIT:
@@ -77,6 +133,7 @@ public class Commands {
     }
 
     public void enroll(String studentID, String courseID){ // This command will help enroll the student specified in the corresponding course. While also updating the money_earned of that course based on its price
+
     }
 
     public void assign(String teacherID,String courseID){   //This command will help assign the teacher specified to the corresponding course
